0b70e54da974ff35537a27078104575d5c8dd5ec
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-12 01:34:39 +0400

    Adding Color Map Green

--- cubes.py
+++ cubes.py
@@ -4,6 +4,6 @@ x_values = range(1,5001)
 y_values = [x**3 for x in x_values]
 
 fig, ax = plt.subplots()
-ax.scatter(x_values,y_values)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Greens, s=10)
 
 plt.show()
