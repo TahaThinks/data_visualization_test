b9bd704338fd0d8317024de1a62cfcdaff522793
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-16 20:54:14 +0400

    Increasing the Number of Points

--- rw_visual.py
+++ rw_visual.py
@@ -3,7 +3,7 @@ from random_walk import RandomWalk
 
 # Make a random walk.
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk
@@ -12,13 +12,16 @@ while True:
     
     point_numbers = range(rw.num_points)
 
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors = 'none', s=20)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors = 'none', s=1)
     
-    # Emphasize the first points
-    ax.scatter(rw.x_values[0], rw.y_values[0], c='green', edgecolors = 'none', s=25)
-    # Emphasize the last points
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors = 'none', s=25)
+    # Emphasize the first points & last points
+    ax.scatter(rw.x_values[0], rw.y_values[0], c='green', edgecolors = 'none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors = 'none', s=100)
     
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
